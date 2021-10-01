4c660d4b2dd50f5bda141a238b7d825f1b9af692
gordonshlai <gordonshlai@yahoo.com.hk>
2021-10-01 19:28:32 +0100

    Displaying posts

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
-import MenuBar from "./component/MenuBar";
+import MenuBar from "./components/MenuBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
